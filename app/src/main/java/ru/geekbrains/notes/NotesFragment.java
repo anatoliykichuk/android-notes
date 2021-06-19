d86798aa7c9977f658ed1f71a291021d1e081a4b
anatoliy.kichuk <anatoliy.kichuk@gmail.com>
2021-06-20 00:09:33 +0300

    Реализовано программный вывод списка заметок.

--- app/src/main/java/ru/geekbrains/notes/NotesFragment.java
+++ app/src/main/java/ru/geekbrains/notes/NotesFragment.java
@@ -2,63 +2,59 @@ package ru.geekbrains.notes;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NotesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NotesFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NotesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NotesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NotesFragment newInstance(String param1, String param2) {
-        NotesFragment fragment = new NotesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notes, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initializeNotes(view);
+    }
+
+    private void initializeNotes(View view) {
+        LinearLayout notesList = (LinearLayout) view;
+        String[] notes = getResources().getStringArray(R.array.notes);
+
+        float textSize = getResources().getDimension(R.dimen.note_item_text_size);
+
+        for (int index = 0; index < notes.length; index++) {
+            TextView noteItem = new TextView(getContext());
+            noteItem.setText(notes[index]);
+            noteItem.setTextSize(TypedValue.COMPLEX_UNIT_SP , textSize);
+
+            notesList.addView(noteItem);
+
+            final int noteIndex = index;
+
+            noteItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showNote(noteIndex);
+                }
+            });
+
+        }
+    }
+
+    private void showNote(int noteIndex) {
+
     }
 }
