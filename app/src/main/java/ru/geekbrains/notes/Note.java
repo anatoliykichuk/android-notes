e847dcda44af6b74b814d93edb09820265e01681
anatoliy.kichuk <anatoliy.kichuk@gmail.com>
2021-06-20 10:28:50 +0300

    Частично реализовано сохранение состояния

--- app/src/main/java/ru/geekbrains/notes/Note.java
+++ app/src/main/java/ru/geekbrains/notes/Note.java
@@ -1,11 +1,57 @@
 package ru.geekbrains.notes;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
-public class Note {
+public class Note implements Parcelable {
+    private int Index;
     private String name;
     private String description;
     private Date dateOfCreation;
 
+    public Note(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.dateOfCreation = new Date();
+    }
 
+    protected Note(Parcel in) {
+        Index = in.readInt();
+        name = in.readString();
+        description = in.readString();
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
+    public int getIndex() {
+        return Index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(Index);
+        dest.writeString(name);
+        dest.writeString(description);
+    }
 }
--- app/src/main/java/ru/geekbrains/notes/NotesFragment.java
+++ app/src/main/java/ru/geekbrains/notes/NotesFragment.java
@@ -1,10 +1,14 @@
 package ru.geekbrains.notes;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -14,6 +18,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class NotesFragment extends Fragment {
+
+    public static final String CURRENT_NOTE = "CurrentNote";
+    private int currentNoteIndex = 0;
+    private boolean isLandscape;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,6 +35,26 @@ public class NotesFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         initializeNotes(view);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(CURRENT_NOTE, currentNoteIndex);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        isLandscape = getResources().getConfiguration().orientation ==
+                Configuration.ORIENTATION_LANDSCAPE;
+
+        if (savedInstanceState != null) {
+            currentNoteIndex = savedInstanceState.getInt(CURRENT_NOTE, 0);
+        }
+
+        showNote(0);
     }
 
     private void initializeNotes(View view) {
@@ -47,6 +75,7 @@ public class NotesFragment extends Fragment {
             noteItem.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    currentNoteIndex = noteIndex;
                     showNote(noteIndex);
                 }
             });
@@ -55,6 +84,27 @@ public class NotesFragment extends Fragment {
     }
 
     private void showNote(int noteIndex) {
+        if (isLandscape) {
+            showNoteNearby(noteIndex);
+        } else {
+            showNoteSeparately(noteIndex);
+        }
+    }
 
+    private void showNoteSeparately(int noteIndex) {
+        Intent intent = new Intent();
+        intent.setClass(getActivity(), NoteActivity.class);
+        intent.putExtra(NoteFragment.INDEX, noteIndex);
+        startActivity(intent);
+    }
+
+    private void showNoteNearby(int noteIndex) {
+        NoteFragment note = NoteFragment.newInstance(noteIndex);
+
+        FragmentManager manager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.note, note);
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.commit();
     }
 }
