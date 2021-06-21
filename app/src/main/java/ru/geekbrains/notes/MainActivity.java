495635a999fd177a7476abbe5009f0bf85139d24
anatoliy.kichuk <anatoliy.kichuk@gmail.com>
2021-06-21 23:02:24 +0300

    Реализован вывод меню и подготовлен метод обработки нажатий.

--- app/src/main/java/ru/geekbrains/notes/MainActivity.java
+++ app/src/main/java/ru/geekbrains/notes/MainActivity.java
@@ -1,9 +1,13 @@
 package ru.geekbrains.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,30 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.notes_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        switch (itemId) {
+            case R.id.menu_add:
+                // TODO: Реализовать добавление заметки.
+                return true;
+            case R.id.menu_edit:
+                // TODO: Реализовать открытие текущей заметки для редактирования.
+                return true;
+            case R.id.menu_remove:
+                // TODO: Реализовать удаление текущей заметки.
+                return true;
+            case R.id.menu_about:
+                // TODO: Реализовать вывод информации о приложении.
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
